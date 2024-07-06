fb3afa83f4e81f6556f9a9f5e4b68197ad008ca8
R-ajesh-R <rajesh.ae171408@gmail.com>
2024-07-06 14:09:38 +0530

    Changed folder name from BackEnd to tmp

--- tmp/server.js
+++ tmp/server.js
@@ -145,15 +145,15 @@ app.post("/api/email", authenticateToken, async (req, res) => {
   }
 });
 
-// app.use(express.static(path.join(__dirname, "../FrontEnd", "dist")));
+app.use(express.static(path.join(__dirname, "../FrontEnd", "dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../FrontEnd", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../FrontEnd", "dist", "index.html"));
+});
 
-app.get('/',(req,res)=>{
-  res.send("Hi There!!");
-})
+// app.get('/',(req,res)=>{
+//   res.send("Hi There!!");
+// })
 
 app.post("/token", (req, res) => {
   const user = uuidv4();
